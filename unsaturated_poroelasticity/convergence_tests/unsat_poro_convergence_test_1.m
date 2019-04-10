3cf4b84acd670e6b7c6ccf6e7709140eebd624f6
Jhabriel Varela <jhabriel@gmail.com>
2019-04-11 01:03:27 +0200

    Matlab script to determine source term for first convergence test

--- unsaturated_poroelasticity/convergence_tests/unsat_poro_convergence_test_1.m
+++ unsaturated_poroelasticity/convergence_tests/unsat_poro_convergence_test_1.m
@@ -9,16 +9,32 @@ syms t     % time
 u = [t*x*(1-x)*y*(1-y), -t*x*(1-x)*y*(1-y)];
 p = -t*x*(1-x)*y*(1-y)-1;
 
+%% Constants 
+C_f = 1;
+C_s = 1;
+K = 1;
+n  = 0.5;
+alpha = 1;
+mu_s = 1;
+lambda_s = 1;
+mu_f = 1;
+
+
 %% Consitutive relationships
 Sw = 1/(1-p);
 krw = p^2;
+C = 1/((1-p)^2);
+
+%% Constant terms
+chi_1 = (alpha-n)*C_s*Sw^2 + n*C_f*Sw;
+chi_2 = ((alpha-n)*C_s*Sw*p + n)*C;
+chi = chi_1 + chi_2;
 
 %% Useful data
 gradient_u = [diff(u(1),x),diff(u(1),y)
               diff(u(2),x),diff(u(2),y)];
 divergence_u = diff(u(1),x) + diff(u(2),y); 
 sw_times_p = Sw * p;
-
 
 gradient_p = [diff(p,x),diff(p,y)];
 gradient_sw_times_p = [diff(sw_times_p,x),diff(sw_times_p,y)];
@@ -36,12 +52,12 @@ div_sigma_eff = [diff(sigma_eff(1,1),x) + diff(sigma_eff(2,1),y),...
                  diff(sigma_eff(1,2),x) + diff(sigma_eff(2,2),y)];
 
 % The momentum equation
-momentum_eq = div_sigma_eff - gradient_sw_times_p;          
+momentum_eq = div_sigma_eff - alpha*gradient_sw_times_p;          
 
 %% Flow terms
 
 % Darcy's law
-qw = - krw * gradient_p;
+qw = - (K/mu_f) * krw * gradient_p;
 
 % Divergence of Darcy's velocity
 div_qw = diff(qw(1),x) + diff(qw(2),y);
@@ -49,8 +65,12 @@ div_qw = diff(qw(1),x) + diff(qw(2),y);
 % Time derivative divergence of u
 time_deriv_div_u = diff(divergence_u,t);
 
+% Time derivative of pressure
+partial_p_partial_t = diff(p,t);
+
 % Mass conservation equation
-mass_eq = Sw * time_deriv_div_u + div_qw;
+mass_eq = chi * partial_p_partial_t ...
+          + alpha * Sw * time_deriv_div_u + div_qw;
 
 %% The source terms
 F = transpose(momentum_eq);
