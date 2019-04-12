52b1ed68e51e2ff18e46ec2b8c8d3bae9f2a8a55
Jhabriel Varela <jhabriel@gmail.com>
2019-04-12 14:25:36 +0200

    Some cleaning of the code - No major changes from last version

--- unsaturated_poroelasticity/convergence_tests/unsat_poro_convergence_test_2.m
+++ unsaturated_poroelasticity/convergence_tests/unsat_poro_convergence_test_2.m
@@ -4,60 +4,64 @@
 syms x     % first cartesian component
 syms y     % second cartesian component
 syms t     % time 
-syms z     % aux
-%% Domain length
-a = 1;
-b = 1;
 
 %% The displacement solution
-u = [t*x*(a-x)*y*(b-y), -t*x*(a-x)*y*(b-y)];
-p = -t*x*(a-x)*y*(b-y)-1;
+u = [t*x*(1-x)*y*(1-y), -t*x*(1-x)*y*(1-y)];
+p = -t*x*(1-x)*y*(1-y)-1;
 
 %% Constants 
 
 % Elasticity
-mu_s = 1;           % [Pa] First Lamé parameter
-lambda_s = 1;       % [Pa] Second Lamé parameter
+mu_s = 1;                   % First Lamé parameter
+lambda_s = 1;               % Second Lamé parameter
 
 % Rock
-n  = 0.5;            % [-] Porosity
-K = 1;               % [m^2] Permeability
-C_s = 1;             % [1/Pa] Solid compressibility
+phi  = 0.4;                 % Porosity
+K = 1;                      % Permeability
+C_s = 1;                    % Solid compressibility
 
 % Fluid
-rho_f = 1;           % [kg/m^3] Density
-g = 1;               % [m/s^2] Gravity acceleration
-mu_f = 1;            % [Pa s] Viscosity
-C_f = 1;             % [1/Pa] Fluid compressibility
+rho_f = 1;                  % Density
+g = 1;                      % Gravity acceleration
+mu_f = 1;                   % Viscosity
+C_f = 1;                    % Fluid compressibility
     
 
 % Medium
-alpha = 1;           % [-] Biot's coefficient
+alpha = 1;                  % Biot's coefficient
 
 % vanGenuchten
-alpha_v = 6;             % [1/m] van Genuchten parameter
-a_v = alpha_v/(rho_f*g); % [ms^2/kg] similar to alpha, but more convinient
-n_v = 1.5;               % [-] van Genuchten parameter
-m_v = 1-(1/n_v);         % [-] van Genuchten parameter
-S_r = 0.25;              % [-] Residual saturation
+alpha_v = 0.04;             % van Genuchten parameter
+a = alpha_v/(rho_f*g);      % similar to alpha, but more convinient
+n = 2;                      % van Genuchten parameter
+m = 1-(1/n);                % van Genuchten parameter
+S_r = 0.25;                 % Residual saturation
 
 %% Consitutive relationships
-S = ((1-S_r)/(1+(a_v*abs(p))^n_v)^m_v) + S_r;
-krw = (1 - (a_v*abs(p))^(n_v-1) * (1 + (a_v*abs(p))^n_v)^(-m_v))^2/(1 + (a_v*abs(p))^n_v)^(m_v/2);
-C = (-m_v*n_v*(1-S_r)*p*(a_v * abs(p))^n_v)/(abs(p)^2 * ((a_v * abs(p))^n_v + 1)^(m_v+1));
+S = ( (1-S_r) / (1+(a*abs(p))^n)^m ) + S_r;
+
+krw = (1 - (a*abs(p) )^(n-1) * (1 + (a*abs(p))^n)^(-m) )^2 / ...
+      (1 + (a*abs(p))^n)^(m/2);
+
+C = (-m * n * (1-S_r) * p * (a * abs(p))^n) / ...
+    (abs(p)^2 * (1 + (a * abs(p))^n)^(m+1));
 
 %% Constant terms
-chi_p = (alpha-n)*C_s*S^2 + n*C_f*S;
-chi_S = (alpha-n)*C_s*S*p + n;
+chi_p = (alpha-phi) * C_s * S^2  +  phi * C_f * S;
+chi_S = ((alpha-phi) * C_s * S * p  +  phi) * C;
 
 %% Useful data
 gradient_u = [diff(u(1),x),diff(u(1),y)
               diff(u(2),x),diff(u(2),y)];
+
 divergence_u = diff(u(1),x) + diff(u(2),y); 
+
 sw_times_p = S * p;
 
 gradient_p = [diff(p,x),diff(p,y)];
+
 gradient_y = [diff(rho_f*g*y,x),diff(rho_f*g*y,y)];
+
 gradient_sw_times_p = [diff(sw_times_p,x),diff(sw_times_p,y)];
 
 %% Mechanical terms
@@ -93,25 +97,11 @@ partial_p_partial_t = diff(p,t);
 partial_s_partial_t = diff(S,t);
 
 % Mass conservation equation
-mass_eq = chi_p * partial_p_partial_t   ...
-          + chi_S * partial_s_partial_t ...
-          + alpha * S * time_deriv_div_u + div_qw;
+mass_eq =   chi_p * partial_p_partial_t   ...
+          + chi_S * partial_p_partial_t ...
+          + alpha * S * time_deriv_div_u ...
+          + div_qw;
 
 %% The source terms
 F = transpose(momentum_eq);
 f = mass_eq;
-
-%%
-G= cartGrid([20,20],[1,1]);
-G = computeGeometry(G);
-x_cntr = G.cells.centroids(:,1);
-y_cntr = G.cells.centroids(:,2);
-
-
-f_fun = matlabFunction(f);
-F_fun = matlabFunction(F);
-
-f_source = f_fun(1,x_cntr,y_cntr);
-F_source = F_fun(1,x_cntr,y_cntr);
-
-S_fun = matlabFunction(S);
